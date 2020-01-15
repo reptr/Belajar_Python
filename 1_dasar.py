b21255ced080561344104166f041d003b60a054e
reptr <restuyoichi@gmail.com>
2020-01-15 10:16:45 +0700

    shebang python linux & windows. dasar pyhton

--- 1_dasar.py
+++ 1_dasar.py
@@ -1,6 +1,7 @@
 # Shebang (untuk menentukan python interpreter), disini saya menggunakan Python 3.8
 # untuk linux
 # #!/usr/local/bin/python3.8
+# lalu menambahkan mode "execute" menggunakan command : chmod +x (nama file)
+
 # untuk windows (sesuaikan dengan nama Path di windowsnya ya!)
 # #!C:\Python38\Python.exe
-
