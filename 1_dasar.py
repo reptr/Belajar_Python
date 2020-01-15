33017927e63e49811433fa8e20e8849c237d43e9
reptr <restuyoichi@gmail.com>
2020-01-15 10:02:05 +0700

    shebang python linux & windows. dasar pyhton

--- 1_dasar.py
+++ 1_dasar.py
@@ -1,6 +1,6 @@
-# Shebang (untuk menentukan python interpreter)
+# Shebang (untuk menentukan python interpreter), disini saya menggunakan Python 3.8
 # untuk linux
 # #!/usr/local/bin/python3.8
-# untuk windows
-# #!C:\Python36\Python.exe
+# untuk windows (sesuaikan dengan nama Path di windowsnya ya!)
+# #!C:\Python38\Python.exe
 
